2cdf54920c94c1094029a343d0dfd57457f220f3
dinkien <21522244@gm.uit.edu.vn>
2024-08-07 23:15:30 +0700

    done update + delete user

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -3,8 +3,10 @@ package vn.hoidanit.laptopshop.controller;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -57,10 +59,48 @@ public class UserController {
 
     }
 
+    // Update user page
+    @RequestMapping(value = "/admin/user/update/{id}", method = RequestMethod.GET)
+    public String getUpdateUserPage(Model model, @PathVariable long id) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "admin/user/update";
+    }
+
+    // Delete user page
+    @GetMapping(value = "/admin/user/delete/{id}")
+    public String getDeleteUserPage(Model model, @PathVariable long id) {
+        model.addAttribute("id", id);
+        User user = new User();
+        user.setId(id);
+        model.addAttribute("newUser", user);
+        return "admin/user/delete";
+    }
+
     // Handle create user
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String handleCreateUser(Model model, @ModelAttribute("newUser") User user) {
         this.userService.handleSaveUser(user);
+        return "redirect:/admin/user";
+    }
+
+    // Handle update user
+    @PostMapping(value = "/admin/user/update")
+    public String handleUpdateUser(Model model, @ModelAttribute("updatedUser") User user) {
+        User currentUser = this.userService.getUserById(user.getId());
+        if (currentUser != null) {
+            currentUser.setFullName(user.getFullName());
+            currentUser.setAddress(user.getAddress());
+            currentUser.setPhone(user.getPhone());
+        }
+        this.userService.handleSaveUser(currentUser);
+        return "redirect:/admin/user";
+    }
+
+    // Handle delete user
+    @PostMapping("/admin/user/delete")
+    public String handleDeleteUSer(Model model, @ModelAttribute("newUser") User user) {
+        this.userService.deleteUserById(user.getId());
         return "redirect:/admin/user";
     }
 }
