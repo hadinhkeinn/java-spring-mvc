212eb72be83a03204bc75cc7d6ffa5a3422981aa
dinkien <21522244@gm.uit.edu.vn>
2024-08-23 15:27:32 +0700

    125 - add to cart

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import vn.hoidanit.laptopshop.domain.Product;
 import vn.hoidanit.laptopshop.service.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ItemController {
@@ -22,4 +26,15 @@ public class ItemController {
         model.addAttribute("product", prd);
         return "client/product/detail";
     }
+
+    // Add product to cart
+    @PostMapping("/add-product-to-cart/{id}")
+    public String handleAddProductToCart(@PathVariable long id, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        String userEmail = (String) session.getAttribute("email");
+        long prdId = id;
+        this.productService.handleAddProductToCart(userEmail, prdId);
+        return "redirect:/";
+    }
+
 }
--- src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
@@ -2,16 +2,29 @@ package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.Cart;
+import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.CartDetailRepository;
+import vn.hoidanit.laptopshop.repository.CartRepository;
 import vn.hoidanit.laptopshop.repository.ProductRepository;
+
 import java.util.List;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
+    private final CartRepository cartRepository;
+    private final CartDetailRepository cartDetailRepository;
+    private final UserService userService;
 
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, CartRepository cartRepository,
+            CartDetailRepository cartDetailRepository, UserService userService) {
         this.productRepository = productRepository;
+        this.cartRepository = cartRepository;
+        this.cartDetailRepository = cartDetailRepository;
+        this.userService = userService;
     }
 
     public void handleSaveProduct(Product product) {
@@ -28,5 +41,34 @@ public class ProductService {
 
     public void deleteProduct(long id) {
         this.productRepository.deleteById(id);
+    }
+
+    public void handleAddProductToCart(String email, long prd_Id) {
+        User user = this.userService.getUserByEmail(email);
+
+        if (user != null) {
+            Cart cart = this.cartRepository.findByUser(user);
+            if (cart == null) {
+                Cart newCart = new Cart();
+                newCart.setUser(user);
+                newCart.setSum(1);
+                cart = this.cartRepository.save(newCart);
+            }
+
+            // Tìm prd by id
+            Product p = this.getProduct(prd_Id);
+
+            if (p != null) {
+                // save cart_detail
+                CartDetail detail = new CartDetail();
+                detail.setCart(cart);
+                detail.setProduct(p);
+                detail.setQuantity(1);
+                detail.setPrice(p.getPrice());
+
+                this.cartDetailRepository.save(detail);
+            }
+
+        }
     }
 }
